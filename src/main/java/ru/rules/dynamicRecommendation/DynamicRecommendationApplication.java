4f5836bc9ab44ab501ec093312b7db761dbac685
Anton-Kostin-r70 <antonkostinr70@gmail.com>
2026-05-13 22:23:58 +0700

    Created the empty project

---
+++ src/main/java/ru/rules/dynamicRecommendation/DynamicRecommendationApplication.java
@@ -0,0 +1,13 @@
+package ru.rules.dynamicRecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicRecommendationApplication.class, args);
+	}
+
+}
